ba7567f7dcec47059bb563359dd20e3beeea659a
Ajay mohite <ajaymohite244@gmail.com>
2021-04-30 02:04:08 +0530

    make it better

---
+++ db.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const isEmail = require("validator").isEmail;
+const bcrypt = require("bcrypt");
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:qMW5b5vdNQr1jpzO@tinderclone.g5v0l.mongodb.net/tindercloneDB?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const schemaopts = {
+  name: { type: String, required: [true, "namenotfound"] },
+  email: {
+    type: String,
+    validate: [isEmail, "notemail"],
+    unique: true,
+    required: [true, "emailnotfound"],
+  },
+  password: {
+    type: String,
+    minlength: [5, "minlength5"],
+    required: [true, "passwordrequired"],
+  },
+  confirmpassword: {
+    type: String,
+    validate: {
+      validator: function (confirmPassword) {
+        // console.log(this.password, confirmPassword);
+        if (this.password === confirmPassword) return true;
+        else {
+          return false;
+        }
+      },
+      message: "passwordnotmatching",
+    },
+    required: [true, "passwordnotmatching"],
+  },
+};
+const dbSchema = new mongoose.Schema(schemaopts);
+
+dbSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 4);
+  this.confirmpassword = this.password;
+  next();
+});
+
+const model = mongoose.model("registerduser", dbSchema);
+
+exports.model = model;
---
+++ server.js
@@ -0,0 +1,51 @@
+const exp = require("express");
+const app = exp();
+const cors = require("cors");
+const dbModel = require("./db").model;
+const bcrypt = require("bcrypt");
+
+app.use(cors());
+app.use(exp.json());
+
+app.get("/", (req, res) => {
+  res.json("Just normal auth server");
+});
+
+app.post("/register", (req, res) => {
+  const user = req.body;
+  dbModel
+    .create(user)
+    .then((doc) => {
+      res.status(201).json("usercreated");
+    })
+    .catch((err) => {
+      res.status(200).json(err.message.split(" ").pop());
+    });
+});
+
+app.post("/login", (req, res) => {
+  const user = req.body;
+
+  if (user.email) {
+    dbModel.findOne({ email: user.email }).then((doc) => {
+      if (doc) {
+        bcrypt.compare(user.password, doc.password, function (err, result) {
+          if (result) {
+            res.status(200).json("userexist");
+          } else {
+            res.status(200).json("passwordwrong");
+          }
+        });
+      } else {
+        res.status(404).json("usernotexist");
+      }
+    });
+  } else {
+    res.status(404).json("usernotexist");
+  }
+});
+
+const PORT = process.env.PORT || 9001;
+app.listen(PORT, () => {
+  console.log("SERVER STARTED");
+});
